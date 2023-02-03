77497088f612708f4b6db291b71573d3f4b436ca
Univolcoe <aaron_jsc@163.com>
2023-02-04 00:15:11 +0800

    Save at 2023 02 03

--- py/test.py
+++ py/test.py
@@ -8,10 +8,3 @@
 ------------      -------    --------    -----------
 2022/7/8 14:28   ishgrina   1.0         None
 '''
-
-# import lib
-from datetime import datetime, timedelta
-from khl.card import CardMessage, Card, Module, Element, Types, Struct
-
-
-import json
