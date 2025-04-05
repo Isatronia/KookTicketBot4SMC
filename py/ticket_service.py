805099862196596c060bd6ba9f4ee7d108d83332
Ishgrina <aaron_jsc@163.com>
2025-04-05 17:49:25 +0800

    temp stash

--- py/ticket_service.py
+++ py/ticket_service.py
@@ -23,7 +23,24 @@ from py.value import PATH
 from entity import Ticket, TKSTAT
 
 
-class TicketService:
+class TicketChannel:
+    def __init__(self, channel: str, guild: str, index: str):
+        self.channel_id = channel
+        self.guild_id = guild
+        self.ticket_index = index
+
+    def __hash__(self):
+        return hash(self.channel_id)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, TicketChannel):
+            return self.channel_id == other.channel_id
+        return False
+
+    def __repr__(self):
+        return f"Ticket Channel({self.channel_id}): [Guild: {self.guild_id}\tIndex: {self.ticket_index}]"
+
+class TicketServiceImpl:
     _ticket_update_lock = Lock()
     _ticket_log = {}
     _ticket_channels = set()
@@ -32,32 +49,48 @@ class TicketService:
         for file_path in glob.glob(os.path.join(PATH.TICKET_DATA_DIRECTORY, "**", "*.json"), recursive=True):
             ticket = Ticket.load_from_file(file_path)
             if ticket:
-                if ticket.guild_id not in TicketService._ticket_log:
-                    TicketService._ticket_log[ticket.guild_id] = {}
-                TicketService._ticket_log[ticket.guild_id][ticket.ticket_id] = ticket
-                TicketService._ticket_channels.add(ticket.channel_id)
+                if ticket.guild_id not in TicketServiceImpl._ticket_log:
+                    TicketServiceImpl._ticket_log[ticket.guild_id] = {}
+                TicketServiceImpl._ticket_log[ticket.guild_id][ticket.ticket_id] = ticket
+                TicketServiceImpl._ticket_channels.add(ticket.channel_id)
 
-    def close_ticket(self, guild_id: int, ticket_id: int):
-        ticket = TicketService._ticket_log[guild_id][ticket_id]
-        ticket.state = TKSTAT.CLOSED
+    async def close_ticket(self, guild_id: int, ticket_id: int):
+        async with self._ticket_update_lock:
+            if isinstance(TicketServiceImpl._ticket_log[guild_id][ticket_id], Ticket):
+                TicketServiceImpl._ticket_log[guild_id][ticket_id].state = TKSTAT.CLOSED
 
-    def open_ticket(self, guild_id: int, ticket_id: int, channel_id: int, applier: int):
-        ticket = Ticket(ticket_id=ticket_id, guild_id=guild_id, channel_id=channel_id, applier=applier,
-                        state=TKSTAT.OPEN, msg_log=[])
-        TicketService._ticket_log[guild_id][ticket_id] = ticket
-        TicketService._ticket_channels.add(channel_id)
+    async def open_ticket(self, guild_id: int, ticket_id: int, channel_id: int, applier: int):
+        async with self._ticket_update_lock:
+            # Create new ticket.
+            ticket = Ticket(ticket_id=ticket_id,
+                            guild_id=guild_id,
+                            channel_id=channel_id,
+                            applier=applier,
+                            state=TKSTAT.OPEN,
+                            msg_log=[])
 
-    def modify_ticket(self, ticket: Ticket = None):
+            TicketServiceImpl._ticket_log[guild_id][ticket_id] = ticket
+            TicketServiceImpl._ticket_channels.add(channel_id)
+
+    async def modify_ticket(self, ticket: Ticket = None):
         if ticket is None:
             return
         if ticket.guild_id is None or ticket.ticket_id is None:
             return
-        temp: Ticket = TicketService._ticket_log[ticket.guild_id][ticket.ticket_id]
-        if temp.channel_id in TicketService._ticket_channels:
-            TicketService._ticket_channels.discard(temp.channel_id)
-            TicketService._ticket_channels.add(ticket.channel_id)
-        temp.update(ticket)
+        async with self._ticket_update_lock:
+            target_ticket: Ticket = TicketServiceImpl._ticket_log[ticket.guild_id][ticket.ticket_id]
+            if target_ticket.channel_id in TicketServiceImpl._ticket_channels:
+                TicketServiceImpl._ticket_channels.discard(target_ticket.channel_id)
+                TicketServiceImpl._ticket_channels.add(ticket.channel_id)
+            target_ticket.update(ticket)
 
-    def on_message(self, message: Message):
-        if str(message.ctx.channel) not in TicketService._ticket_channels:
+    async def on_message(self, message: Message):
+        if str(message.ctx.channel) not in TicketServiceImpl._ticket_channels:
             return
+        async with self._ticket_update_lock:
+            ticket_info = self._ticket_channels.get(str(message.ctx.channel))
+            guild_id, ticket_id = ticket_info.guild_id, ticket_info.ticket_index
+            self._ticket_log[guild_id][ticket_id].msg_log.append(message.content)
+
+
+ticket_service = TicketServiceImpl()
